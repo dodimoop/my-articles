4d36b973595905bfead49e55504fbb86dd9c5146
dodimoop <dody.setiyawan11@gmail.com>
2019-10-31 02:27:28 +0700

    Add style and showing field

--- src/pages/addNewPost.js
+++ src/pages/addNewPost.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 
+// import component which you will use
+import Input from '../components/input'
+
 const useStyles = createUseStyles({
+  addNewPost: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
   title: {
     textAlign: 'center'
+  },
+  inputTitle: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '50%',
+    padding: '10px',
+    margin: '10px'
+  },
+  textareaWrapper: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  textarea: {
+    width: '50%',
+    padding: '10px',
+    margin: '10px',
+    height: '100px'
   }
 })
 
@@ -13,6 +37,11 @@ const AddNewPost = () => {
   return (
     <div>
       <h3 className={classes.title}>ADD NEW POST</h3>
+      <Input className={classes.inputTitle} placeholder="Input title" />
+      <div className={classes.textareaWrapper}>
+        <textarea className={classes.textarea} placeholder="Input content" />
+      </div>
+      <Input className={classes.inputTitle} placeholder="Input author" />
     </div>
   )
 }
