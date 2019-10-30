eb354de325f9f4d0c6b853de19d5d332bb851f93
dodimoop <dody.setiyawan11@gmail.com>
2019-10-30 23:57:09 +0700

    Add Switch, Route and Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 // import component which you will use
 import Header from './components/headers'
+import PostsList from './pages/postsList'
+import AddNewPost from './pages/addNewPost'
+import PasswordGenerator from './pages/passwordGenerator'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Switch>
+        <Route path="/" exact component={PostsList} />
+        <Route path="/add-new-post" exact component={AddNewPost} />
+        <Route path="/password-generator" exact component={PasswordGenerator} />
+      </Switch>
     </div>
   )
 }
