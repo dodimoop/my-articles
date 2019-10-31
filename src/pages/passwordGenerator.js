3bdb6aaff245aab14aeb750a8c5541ba2f7616d7
dodimoop <dody.setiyawan11@gmail.com>
2019-10-31 20:30:27 +0700

    Make a password generator

--- src/pages/passwordGenerator.js
+++ src/pages/passwordGenerator.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
+
+// import component which you will use
+import Input from '../components/input'
+import Button from '../components/button'
 
 const useStyles = createUseStyles({
   paper: {
     boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
     background: '#fff',
     margin: '1rem',
-    padding: 20,
+    padding: 10,
     width: '95%'
   },
   title: {
@@ -17,9 +21,34 @@ const useStyles = createUseStyles({
 const PasswordGenerator = () => {
   const classes = useStyles()
 
+  // getter and setter
+  const [password, setPassword] = useState('')
+
+  // Method for creating a password generator
+  const buttonPassword = () => {
+    let resultPassword = ''
+    const mixCharacters =
+      '+-tu!)@#cdefghiSTUV<~>ab%^&$jkqr*(svwq56LMyz0189ABCD27EFGHIJK34NOPQRlmnopWQYX'
+    for (let i = 0; i <= 11; i += 1) {
+      const random = Math.floor(Math.random() * mixCharacters.length)
+      resultPassword += mixCharacters.charAt(random)
+    }
+    setPassword(resultPassword)
+  }
+
   return (
     <div className={classes.paper}>
       <h3 className={classes.title}>PASSWORD GENERATOR</h3>
+      <div>
+        <Input
+          type="text"
+          className={classes.input}
+          placeholder="Password generator"
+          value={password}
+          readOnly
+        />
+        <Button onClick={() => buttonPassword()}>Password Generator</Button>
+      </div>
     </div>
   )
 }
