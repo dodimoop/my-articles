343770eec8b52e690b9729dc3d332f02ee2e23b0
dodimoop <dody.setiyawan11@gmail.com>
2019-10-31 18:55:29 +0700

    Adding paper

--- src/pages/passwordGenerator.js
+++ src/pages/passwordGenerator.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
+  paper: {
+    boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+    background: '#fff',
+    margin: '1rem',
+    padding: 20,
+    width: '95%'
+  },
   title: {
     textAlign: 'center'
   }
@@ -11,7 +18,7 @@ const PasswordGenerator = () => {
   const classes = useStyles()
 
   return (
-    <div>
+    <div className={classes.paper}>
       <h3 className={classes.title}>PASSWORD GENERATOR</h3>
     </div>
   )
