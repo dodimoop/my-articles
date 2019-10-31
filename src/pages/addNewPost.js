a3242740bc61361a347c8bbfde52dbf8f7ecf99f
dodimoop <dody.setiyawan11@gmail.com>
2019-11-01 01:44:56 +0700

    Add  new post

--- src/pages/addNewPost.js
+++ src/pages/addNewPost.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { maxBy } from 'lodash'
 
 // import component which you will use
 import Input from '../components/input'
 import Button from '../components/button'
+import dataJson from '../data/data'
 
 const useStyles = createUseStyles({
   paper: {
@@ -29,23 +33,107 @@ const useStyles = createUseStyles({
     padding: '10px',
     margin: '10px',
     height: '100px'
+  },
+  errors: {
+    color: 'red',
+    margin: 0,
+    textAlign: 'center'
   }
 })
 
-const AddNewPost = () => {
+const AddNewPost = ({ history }) => {
   const classes = useStyles()
+
+  // data.json
+  const dataExisting = [...dataJson]
+
+  // getter and setter
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  // method when on input title
+  const onTitleInput = event => {
+    const resTitle = event.target.value
+    setTitle(resTitle)
+  }
+
+  // method when on input body
+  const onBodyInput = event => {
+    const resBody = event.target.value
+    setBody(resBody)
+  }
+
+  // method when on input author
+  const onAuthorInput = event => {
+    const resAuthor = event.target.value
+    setAuthor(resAuthor)
+  }
+
+  // method when on click button add post
+  const onAddPost = () => {
+    setIsSubmitting(true)
+
+    // handle if null
+    if (title === '' || body === '' || author === '') {
+      return
+    }
+
+    // handle get last id
+    const getMaxId = maxBy(dataJson, o => {
+      return o.id
+    })
+
+    // new data
+    const newDataPost = {
+      author,
+      id: getMaxId.id + 1,
+      title,
+      body
+    }
+
+    dataExisting.push(newDataPost)
+    history.push('/')
+  }
 
   return (
     <div className={classes.paper}>
       <h3 className={classes.title}>ADD NEW POST</h3>
-      <Input placeholder="Input title" />
+      {title === '' && isSubmitting && (
+        <p className={classes.errors}>Title is required.</p>
+      )}
+      <Input
+        placeholder="Input title"
+        value={title}
+        onChange={event => onTitleInput(event)}
+      />
+      {body === '' && isSubmitting && (
+        <p className={classes.errors}>Body is required.</p>
+      )}
       <div className={classes.textareaWrapper}>
-        <textarea className={classes.textarea} placeholder="Input content" />
+        <textarea
+          className={classes.textarea}
+          placeholder="Input content"
+          value={body}
+          onChange={event => onBodyInput(event)}
+        />
       </div>
-      <Input placeholder="Input author" />
-      <Button>Submit</Button>
+      {author === '' && isSubmitting && (
+        <p className={classes.errors}>Author is required.</p>
+      )}
+      <Input
+        placeholder="Input author"
+        value={author}
+        onChange={event => onAuthorInput(event)}
+      />
+      <Button onClick={() => onAddPost()}>Add Post</Button>
     </div>
   )
 }
 
-export default AddNewPost
+AddNewPost.propTypes = {
+  history: PropTypes.object.isRequired
+}
+
+export default withRouter(AddNewPost)
